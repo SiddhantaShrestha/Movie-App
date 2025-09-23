df4d8135d4b65a4e445d900c249477ca7e110c31
SiddhantaShrestha <siddhanta.shrestha2004@gmail.com>
2025-09-23 13:07:42 +0545

    pages deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,11 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+  // IMPORTANT: project page uses /Movie-App/ as base
+  base: '/Movie-App/',
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'docs', // GitHub Pages will serve from /docs on main
+    emptyOutDir: true,
+  },
 });
